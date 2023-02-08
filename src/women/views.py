98b3315ef29542addff60e4dee66aa2c164f8a05
DonOutcast <lymondgl@student.21-school.ru>
2023-02-08 14:42:56 +0300

    my road to web application via django freamwork

--- src/women/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-# Create your views here.
-def index(request):
-    return HttpResponse("Hello World")
-
-def categori(request, catid):
-    return HttpResponse("<h1>Don Outcast </h1>")
