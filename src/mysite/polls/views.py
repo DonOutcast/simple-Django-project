dfc2432137fdb37534327ababb11a3db6a30d691
DonOutcast <lymondgl@student.21-school.ru>
2023-02-12 15:02:57 +0300

    add space app

--- src/mysite/polls/views.py
+++ src/mysite/polls/views.py
@@ -18,10 +18,7 @@ def home(request):
 
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/detail.html", {"question": question})
 
 
