7ce4b03204b9800dfdf22e493f605d0f26e5e516
DonOutcast <lymondgl@student.21-school.ru>
2023-02-12 13:30:27 +0300

    added a simple template

--- src/mysite/polls/views.py
+++ src/mysite/polls/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
+from django.template import loader
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template("polls/index.html")
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 
 def home(request):
