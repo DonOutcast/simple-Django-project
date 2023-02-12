387a1c0f68f9a681b1b42ba75347b467d4b41306
DonOutcast <lymondgl@student.21-school.ru>
2023-02-12 14:17:17 +0300

    use funciton render

--- src/mysite/polls/views.py
+++ src/mysite/polls/views.py
@@ -6,11 +6,10 @@ from django.template import loader
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template("polls/index.html")
     context = {
         "latest_question_list": latest_question_list,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, "polls/index.html", context)
 
 
 def home(request):
